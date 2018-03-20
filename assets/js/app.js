142710776bd8921aed8b2c0488b0dfaf6005934e
Lisa Jetton <ldjetton21@gmail.com>
2018-03-20 08:40:31 -0500

    completed expereince section, updated responsiveness for profpic, and worked more on sticky video.

--- assets/js/app.js
+++ assets/js/app.js
@@ -156,7 +156,7 @@ $(document).ready( function() { // makes sure the whole site is loaded
 
 
 //////////////////////////////////////////////////////////////////////////
-  // Start Video Control Logic
+  // Start Video Control Logic >> currently in the index.html script tag section <<
 //////////////////////////////////////////////////////////////////////////
   // const video = $("#video-player").get(0);
 
@@ -181,7 +181,7 @@ $(document).ready( function() { // makes sure the whole site is loaded
 
 //HTML5 Video Scroll Logic
 ////////////////////////////////
-var currentWindow = $( window ); // The CURRNT window Object.
+var currentWindow = $( window ); // The CURRENT window Object.
 var featuredMedia = $( "#video-player" ); // The Video Container.
 var featuredVideo = $( "#featured-video" ); // The Video Source.
  
@@ -190,9 +190,9 @@ var offset = Math.floor( topPosition + ( featuredMedia.outerHeight() / 2 ) ); //
 
 
 currentWindow
-.on( "resize", function() {
-  top = featuredMedia.offset().top;
-  offset = Math.floor( top + ( featuredMedia.outerHeight() / 2 ) );
+  .on( "resize", function() {
+    top = featuredMedia.offset().top;
+    offset = Math.floor( top + ( featuredMedia.outerHeight() / 2 ) );
 })
 // .on( "scroll", function() {
 //   featuredVideo.addClass( "is-sticky",
@@ -207,13 +207,15 @@ currentWindow
 //////////////////////////////////////////////////////////////////////////
   function viewCard() {
     console.log("clicked the viewcard");
-    $("#viewCard").hide();
-    $("#closeCard").show();
+    // $(".viewCard").hide();
+    $(".cardTitle").hide();
+    // $(".closeCard").show();
   };
 
   function closeCard() {
-    $("#viewCard").show();
-    $("#closeCard").hide();
+    // $(".viewCard").show();
+    $(".cardTitle").show();
+    // $(".closeCard").hide();
   };
 
 });
@@ -350,3 +352,49 @@ currentWindow
 // }
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
+
+//  =====================
+// // SKILLS PHOTO SCROLLER
+// // =====================
+// $(function(){
+//         var scroller = $('#scroller div.innerScrollArea');
+//         var scrollerContent = scroller.children('ul');
+//         scrollerContent.children().clone().appendTo(scrollerContent);
+//         var curX = 0;
+//         scrollerContent.children().each(function(){
+//             var $this = $(this);
+//             $this.css('left', curX);
+//             curX += $this.outerWidth(true);
+//         });
+//         var fullW = curX / 2;
+//         var viewportW = scroller.width();
+
+//         // Scrolling speed management
+//         var controller = {curSpeed:0, fullSpeed:2};
+//         var $controller = $(controller);
+//         var tweenToNewSpeed = function(newSpeed, duration)
+//         {
+//             if (duration === undefined)
+//                 duration = 600;
+//             $controller.stop(true).animate({curSpeed:newSpeed}, duration);
+//         };
+
+//         // Pause on hover
+//         scroller.hover(function(){
+//             tweenToNewSpeed(0);
+//         }, function(){
+//             tweenToNewSpeed(controller.fullSpeed);
+//         });
+
+//         // Scrolling management; start the automatical scrolling
+//         var doScroll = function()
+//         {
+//             var curX = scroller.scrollLeft();
+//             var newX = curX + controller.curSpeed;
+//             if (newX > fullW*2 - viewportW)
+//                 newX -= fullW;
+//             scroller.scrollLeft(newX);
+//         };
+//         setInterval(doScroll, 20);
+//         tweenToNewSpeed(controller.fullSpeed);
+//     });
